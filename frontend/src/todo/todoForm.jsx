6df07ea4d553635669d3dd83830146715caf40ce
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-03-15 11:59:24 -0300

    Criando actionCreator para adicionar tarefa. Foi instalado os middleware redux-multi e redux-thunk. Com o redux-multi é possível retornar mais de uma action em um array em um actionCreator Com o redux-thunk é possível retornar uma action apenas após terminar uma tarefa, tbm é possível retornar várias actions pois as actions são retornadas ao executar o método dispatch

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-import { changeDescription, search } from './todoActions';
+import { add, changeDescription, search } from './todoActions';
 
 class TodoForm extends Component {
   constructor(props) {
@@ -18,28 +18,30 @@ class TodoForm extends Component {
   }
 
   keyHandler(e) {
+    const { add, search, description } = this.props
     if (e.key === 'Enter') {
-      e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+      e.shiftKey ? search() : add(description)
     } else if (e.key === 'Escape') {
       this.props.handleClear()
     }
   }
 
   render() {
+    const { add, changeDescription, search, description } = this.props
     return (
       <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
           <input id='description'
             className='form-control'
             placeholder='Adicione uma tarefa'
-            value={this.props.description}
-            onChange={this.props.changeDescription}></input>
+            value={description}
+            onChange={changeDescription}></input>
         </Grid>
         <Grid cols='12 3 2'>
           <IconButton style='primary' icon='plus'
-            onClick={this.props.handleAdd}></IconButton>
+            onClick={() => add(description)}></IconButton>
           <IconButton style='info' icon='search'
-            onClick={this.props.handleSearch}></IconButton>
+            onClick={() => search()}></IconButton>
           <IconButton style='default' icon='close'
             onClick={this.props.handleClear}></IconButton>
         </Grid>
@@ -52,6 +54,6 @@ const mapStateToProps = state => ({
   description: state.todo.description,
 })
 
-const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription, search }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
